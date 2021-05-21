2e5a5a798eeb31514ce668d55487e111993e5030
Jonathan LEI <xJonathan@outlook.com>
2021-05-21 04:21:55 +0000

    Change MetaMask to BetterMask

--- ui/app/components/ui/mascot/mascot.component.js
+++ ui/app/components/ui/mascot/mascot.component.js
@@ -1,7 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { createRef, Component } from 'react';
-import MetaMaskLogo from '@metamask/logo';
-import { debounce } from 'lodash';
+import React, { Component } from 'react';
 
 const directionTargetGenerator = ({ top, left, height, width }) => {
   const horizontalMiddle = left + width / 2;
@@ -35,93 +33,17 @@ export default class Mascot extends Component {
 
   constructor(props) {
     super(props);
-
-    const { width, height, followMouse } = props;
-
-    this.logo = MetaMaskLogo({
-      followMouse,
-      pxNotRatio: true,
-      width,
-      height,
-    });
-
-    this.mascotContainer = createRef();
-
-    this.refollowMouse = debounce(
-      this.logo.setFollowMouse.bind(this.logo, true),
-      1000,
-    );
-    this.unfollowMouse = this.logo.setFollowMouse.bind(this.logo, false);
-  }
-
-  handleAnimationEvents() {
-    // only setup listeners once
-    if (this.animations) {
-      return;
-    }
-    this.animations = this.props.animationEventEmitter;
-    this.animations.on('point', this.lookAt.bind(this));
-    this.animations.on(
-      'setFollowMouse',
-      this.logo.setFollowMouse.bind(this.logo),
-    );
-  }
-
-  lookAt(target) {
-    this.unfollowMouse();
-    this.logo.lookAt(target);
-    this.refollowMouse();
-  }
-
-  componentDidMount() {
-    this.mascotContainer.current.appendChild(this.logo.container);
-    this.directionTargetMap = directionTargetGenerator(
-      this.mascotContainer.current.getBoundingClientRect(),
-    );
-
-    const { lookAtTarget, lookAtDirection } = this.props;
-
-    if (lookAtTarget?.x && lookAtTarget?.y) {
-      this.logo.lookAtAndRender(lookAtTarget);
-    } else if (lookAtDirection) {
-      this.logo.lookAtAndRender(this.directionTargetMap[lookAtDirection]);
-    }
-  }
-
-  componentDidUpdate(prevProps) {
-    const {
-      lookAtTarget: prevTarget = {},
-      lookAtDirection: prevDirection = null,
-      followMouse: prevFollowMouse,
-    } = prevProps;
-    const { lookAtTarget = {}, followMouse, lookAtDirection } = this.props;
-
-    if (lookAtDirection && prevDirection !== lookAtDirection) {
-      this.logo.lookAtAndRender(this.directionTargetMap[lookAtDirection]);
-    } else if (
-      lookAtTarget?.x !== prevTarget?.x ||
-      lookAtTarget?.y !== prevTarget?.y
-    ) {
-      this.logo.lookAtAndRender(lookAtTarget);
-    }
-    if (prevFollowMouse !== followMouse) {
-      this.unfollowMouse();
-      followMouse && this.refollowMouse();
-    }
-  }
-
-  componentWillUnmount() {
-    this.animations = this.props.animationEventEmitter;
-    this.animations.removeAllListeners();
-    this.logo.container.remove();
-    this.logo.stopAnimation();
   }
 
   render() {
-    // this is a bit hacky
-    // the event emitter is on `this.props`
-    // and we dont get that until render
-    this.handleAnimationEvents();
-    return <div ref={this.mascotContainer} style={{ zIndex: 0 }} />;
+    return (
+      <img
+        id="logo"
+        src="./images/logo/bettermask-fox.svg"
+        alt="logo"
+        width="175"
+        height="165"
+      />
+    );
   }
 }
